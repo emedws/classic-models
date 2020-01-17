8ab0ce54cb3449940986746ba2496111ede49860
emediano <emmanuel.mediano@autozone.com>
2020-01-17 14:28:18 -0700

    Initial commit

---
+++ src/test/java/mx/emmanuel/classicmodels/ClassicmodelsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.emmanuel.classicmodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicmodelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
